1e17573c2d626a906181ec2661c9d3677ffab5ac
kamaboko1 <github65@hotmail.com>
2019-06-11 11:38:49 -0700

    added sqlite and knex

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// bring in knex
+const knex = require('knex')
+
+// setup config for knex
+const config = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.db3'
+  },
+  useNullAsDefault: true
+}
+
+// set knex config to db
+const db = knex(config)
+
 const express = require('express');
 const helmet = require('helmet');
 
@@ -7,6 +22,9 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+
+
 
 const port = 3300;
 server.listen(port, function() {
