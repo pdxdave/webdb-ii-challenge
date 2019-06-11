a60757e35b268c79311890a482f5ea3cebd02bcc
kamaboko1 <github65@hotmail.com>
2019-06-11 12:19:57 -0700

    added beards to db finished basic CRUD

--- index.js
+++ index.js
@@ -24,6 +24,78 @@ server.use(helmet());
 // endpoints here
 
 
+// GET all animals
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+  .then(zoos => {
+    res.status(200).json(zoos)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
+})
+
+// GET animals by ID
+server.get('/api/zoos/:id', (req, res) => {
+  db('zoos')
+  .where({id: req.params.id})
+  .then(zoos => {
+    res.status(200).json(zoos)
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  });
+})
+
+// POST  a new animal
+server.post('/api/zoos', (req, res) => {
+  db('zoos')
+  .insert(req.body, 'id')
+  .then(ids => {
+    res.status(201).json(ids)
+  })
+  .catch(error => {
+    res.status(500).json(ids)
+  })
+})
+
+// UPDATE an animal
+server.put('/api/zoos/:id', (req, res) => {
+   const change = req.body
+   db('zoos')
+   .where({id: req.params.id})
+   .update(change)
+   .then(count => {
+     if (count > 0) {
+       res.status(200).json({
+         message: (`${count} records updated`)
+       })
+     } else {
+       res.status(404).json({
+         message: "Could not update"
+       })
+     }
+   })
+});
+
+
+// REMOVE an animal
+server.delete('/api/zoos/:id', (req, res) => {
+  db('zoos')
+  .where({ id: req.params.id})
+  .del()
+  .then(count => {
+    if (count > 0) {
+      res.status(200).json({
+        message: `${count} records deleted`
+      })
+    } else {
+      res.status(404).json({
+        message: "Not found"
+      })
+    }
+  })
+});
 
 
 const port = 3300;
